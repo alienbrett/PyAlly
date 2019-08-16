d4b5d8101d5d521c52ceb669f6373c674775163d
Brett Graves <alienbrett648@gmail.com>
2019-08-16 15:56:11 -0500

    Option orders working!

--- tests.py
+++ tests.py
@@ -2,23 +2,32 @@ import sys
 import ally
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 n_tests = 3
 =======
 n_tests = 5
 >>>>>>> updates
+=======
+n_tests = 3
+>>>>>>> Option orders working!
 tests = range(n_tests)
 
 def Test(t):
     t = int(t)
 <<<<<<< HEAD
+<<<<<<< HEAD
     print("TEST " + str(t))
 =======
 #     print("TEST " + str(t))
 >>>>>>> updates
+=======
+    print("TEST " + str(t))
+>>>>>>> Option orders working!
     a = ally.Ally()
     
     
     if t == 1:
+<<<<<<< HEAD
 <<<<<<< HEAD
         orders = [
             ally.order.Order(
@@ -34,18 +43,21 @@ def Test(t):
         print(instrument)
         op = ally.instrument.Put(instrument, "2019-10-18", 55)
         print(op)
+=======
+>>>>>>> Option orders working!
         orders = [
             ally.order.Order(
-                timespan=ally.order.Timespan('GTC'),
-                price=ally.order.Market(),
-                type=ally.order.Buy()
-            ),
-            ally.order.Order(
+                instrument=ally.instrument.Equity('spy'),
+                quantity=ally.order.Quantity(100),
                 timespan=ally.order.Timespan('day'),
-                price=ally.order.Stop(100.0),
-                type=ally.order.Sell(False)
+                type=ally.order.Buy(),
+                price=ally.order.Market()
             )
+<<<<<<< HEAD
 >>>>>>> updates
+=======
+            
+>>>>>>> Option orders working!
         ]
 
         for order in orders:
@@ -69,11 +81,16 @@ def Test(t):
         
     elif t == 2:
 <<<<<<< HEAD
+<<<<<<< HEAD
             
             
 =======
              
 >>>>>>> updates
+=======
+            
+            
+>>>>>>> Option orders working!
 
         print(ally.utils.option_format("ibm", "2014-01-18", 200.00, "call"))
         print(ally.utils.option_format())
@@ -83,10 +100,14 @@ def Test(t):
     elif t == 3:
 
 <<<<<<< HEAD
+<<<<<<< HEAD
         a.holdings_chart('graph.png')
 =======
         print(a.holdings_chart('graph.png'))
 >>>>>>> updates
+=======
+        a.holdings_chart('graph.png')
+>>>>>>> Option orders working!
 
             
             
@@ -94,6 +115,7 @@ def Test(t):
             
 
     elif t == 4:
+<<<<<<< HEAD
 <<<<<<< HEAD
         a.get_quote('nvda','bid,ask')
         a.get_quote('nvda,chk,brk.b','bid,ask,vol')
@@ -105,76 +127,15 @@ def Test(t):
         print(a.get_quote('nvda','bid,ask'))
         print(a.get_quote('nvda,chk,brk.b','bid,ask,vol'))
         print(a.get_quote(['nvda','chk,brk.b'],['bid','ask','vol']))
+=======
+        a.get_quote('nvda','bid,ask')
+        a.get_quote('nvda,chk,brk.b','bid,ask,vol')
+>>>>>>> Option orders working!
         a.get_quote('','')
+        a.get_quote()
 
-    
-    elif t == 5:
-#         print(a.get_strike_prices('nvda'))
-#         print(a.get_strike_prices('chk'))
-#         print(a.get_exp_dates('aapl'))
-#         print(a.get_exp_dates('nvda'))
 
-        print("CALLS")
-        op_chain = a.options_chain(
-            symbol="spy",
-            exp_date="20191018",
-            direction="c"
-        )
-        for op in op_chain:
-            print(op['issue_desc'] +  "\t( $" + str(op['ask']).rjust(5,' ') + " )" \
-                  + ("(ITM)" if op['in_the_money'] else "")\
-                 )
-            
-        print("PUTS")
-        op_chain = a.options_chain(
-            symbol="spy",
-            exp_date="20191018",
-            direction="p"
-        )
-        for op in op_chain:
-            print(op['issue_desc'] +  "\t( $" + str(op['ask']).rjust(5,' ') + " )" \
-                  + ("(ITM)" if op['in_the_money'] else "".rjust(4,' ')) \
-                 )
-        
-        ops = a.search_options(
-            symbol="spy",
-            query="xdate-eq:20191018 AND put_call-eq:call",
-            fields="bid,ask,vol"
-        )
-        
-        
-        
-        
-        
-    elif t == 6:
-        
-        
-        
-        
-        dates = a.get_exp_dates("spy")
-        print(dates)
-            
-            
-            
-            
-            
-            
-            
-    elif t == 7:
-        
-        
-        instrument = ally.instrument.Equity('SPY')
-        op = ally.instrument.Put(instrument, "2019-10-18", 291)
-        
-        
-        order = ally.order.Order(
-            timespan=ally.order.Timespan('day'),
-            price=ally.order.Market(),
-            type=ally.order.Buy(),
-            instrument=instrument,
-            quantity=ally.order.Quantity(1)
-        )
-
+<<<<<<< HEAD
         print(
             a.submit_order(
                 order=order,
@@ -185,6 +146,8 @@ def Test(t):
                    
         
 >>>>>>> updates
+=======
+>>>>>>> Option orders working!
 
 if len(sys.argv) < 2:
     print("Running all tests:")
