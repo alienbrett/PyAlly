098615084cc05268163e6afb64a264b6dcdfe773
Brett Graves <alienbrett648@gmail.com>
2019-08-22 13:33:50 -0500

    Latest

--- tests.py
+++ tests.py
@@ -1,50 +1,24 @@
 import sys
 import ally
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-n_tests = 3
-=======
 n_tests = 5
->>>>>>> updates
-=======
-n_tests = 3
->>>>>>> Option orders working!
 tests = range(n_tests)
 
 def Test(t):
     t = int(t)
-<<<<<<< HEAD
-<<<<<<< HEAD
     print("TEST " + str(t))
-=======
-#     print("TEST " + str(t))
->>>>>>> updates
-=======
-    print("TEST " + str(t))
->>>>>>> Option orders working!
     a = ally.Ally()
     
     
     if t == 1:
-<<<<<<< HEAD
-<<<<<<< HEAD
-        orders = [
-            ally.order.Order(
-                instrument=ally.instrument.Equity('spy'),
-                quantity=ally.order.Quantity(100),
-                timespan=ally.order.Timespan('day'),
-                type=ally.order.Buy(),
-                price=ally.order.Market()
-            )
-            
-=======
+        
         instrument = ally.instrument.Equity('TSLA')
         print(instrument)
         op = ally.instrument.Put(instrument, "2019-10-18", 55)
         print(op)
-=======
->>>>>>> Option orders working!
+        
+        
+        
         orders = [
             ally.order.Order(
                 instrument=ally.instrument.Equity('spy'),
@@ -53,11 +27,6 @@ def Test(t):
                 type=ally.order.Buy(),
                 price=ally.order.Market()
             )
-<<<<<<< HEAD
->>>>>>> updates
-=======
-            
->>>>>>> Option orders working!
         ]
 
         for order in orders:
@@ -80,17 +49,6 @@ def Test(t):
         
         
     elif t == 2:
-<<<<<<< HEAD
-<<<<<<< HEAD
-            
-            
-=======
-             
->>>>>>> updates
-=======
-            
-            
->>>>>>> Option orders working!
 
         print(ally.utils.option_format("ibm", "2014-01-18", 200.00, "call"))
         print(ally.utils.option_format())
@@ -99,15 +57,7 @@ def Test(t):
 
     elif t == 3:
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        a.holdings_chart('graph.png')
-=======
         print(a.holdings_chart('graph.png'))
->>>>>>> updates
-=======
-        a.holdings_chart('graph.png')
->>>>>>> Option orders working!
 
             
             
@@ -115,27 +65,13 @@ def Test(t):
             
 
     elif t == 4:
-<<<<<<< HEAD
-<<<<<<< HEAD
-        a.get_quote('nvda','bid,ask')
-        a.get_quote('nvda,chk,brk.b','bid,ask,vol')
-        a.get_quote('','')
-        a.get_quote()
-
-
-=======
         print(a.get_quote('nvda','bid,ask'))
         print(a.get_quote('nvda,chk,brk.b','bid,ask,vol'))
         print(a.get_quote(['nvda','chk,brk.b'],['bid','ask','vol']))
-=======
-        a.get_quote('nvda','bid,ask')
-        a.get_quote('nvda,chk,brk.b','bid,ask,vol')
->>>>>>> Option orders working!
         a.get_quote('','')
         a.get_quote()
 
 
-<<<<<<< HEAD
         print(
             a.submit_order(
                 order=order,
@@ -145,9 +81,6 @@ def Test(t):
         )
                    
         
->>>>>>> updates
-=======
->>>>>>> Option orders working!
 
 if len(sys.argv) < 2:
     print("Running all tests:")
