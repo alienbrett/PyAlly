3843cf59c8c41482396496899cbbf89f8131c2ee
Brett Graves <alienbrett648@gmail.com>
2020-06-20 16:45:45 -0500

    Removed some trailing whitespace

--- ally/Ally.py
+++ ally/Ally.py
@@ -1,17 +1,17 @@
 # MIT License
-# 
+#
 # Copyright (c) 2020 Brett Graves
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,7 +31,7 @@ from .Watchlist	import Watchlist
 
 
 
-_all_params = ( 
+_all_params = (
 	'ALLY_OAUTH_SECRET',
 	'ALLY_OAUTH_TOKEN',
 	'ALLY_CONSUMER_SECRET',
@@ -121,7 +121,7 @@ class Ally:
 
 	.. code-block:: python
 
-				params = { 
+				params = {
 					'ALLY_OAUTH_SECRET': ...,
 					'ALLY_OAUTH_TOKEN': ...,
 					'ALLY_CONSUMER_SECRET': ...,
