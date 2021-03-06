16a89a2b005c87bf9353cc6b44eff8c49e11bc5c
Matt Margolin <matt.margolin@gmail.com>
2021-03-05 22:28:53 -0800

    black + isort

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -1,5 +1,3 @@
-import sphinx_rtd_theme
-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -14,17 +12,18 @@ import sphinx_rtd_theme
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'PyAlly'
-copyright = '2020, Brett Graves'
-author = 'Brett Graves'
+project = "PyAlly"
+copyright = "2020, Brett Graves"
+author = "Brett Graves"
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.1'
+release = "1.1.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -33,19 +32,19 @@ release = '1.1.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	'sphinx.ext.autodoc',
-	'sphinx.ext.coverage',
-	'sphinx.ext.napoleon',
-	'sphinx_rtd_theme'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -53,9 +52,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
--- test.py
+++ test.py
@@ -1,28 +1,25 @@
-import ally
-import json
-import time
 import concurrent.futures
+
+import ally
 
 a = ally.Ally()
 
 # print(h)
-def job ():
-	print("Submitting job")
-	return a.timesales('spy', startdate='2020-06-19', enddate='2020-06-19', block=False)
+def job():
+    print("Submitting job")
+    return a.timesales("spy", startdate="2020-06-19", enddate="2020-06-19", block=False)
+
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
 
-	print("Submitting requests")
-	# Submit all our orders
-	futures = {
-		executor.submit(job): i
-		for i in range(200)
-	}
-	print("Submitted!")
-	
-	print("Getting results...")
-	for future in concurrent.futures.as_completed(futures):
-		try:
-			print(future.result(), "#{}".format(futures[future]))
-		except:
-			raise
+    print("Submitting requests")
+    # Submit all our orders
+    futures = {executor.submit(job): i for i in range(200)}
+    print("Submitted!")
+
+    print("Getting results...")
+    for future in concurrent.futures.as_completed(futures):
+        try:
+            print(future.result(), "#{}".format(futures[future]))
+        except:
+            raise
